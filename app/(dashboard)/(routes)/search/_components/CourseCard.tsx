95a2afec38edb59f25fe617d73f91bffadc878e4
Brien-Austin <brienaustinclayton@gmail.com>
2024-01-26 13:32:51 +0530

    fixed build errors

--- app/(dashboard)/(routes)/search/_components/CourseCard.tsx
+++ app/(dashboard)/(routes)/search/_components/CourseCard.tsx
@@ -1,50 +1,72 @@
+import Image from "next/image";
+import Link from "next/link";
+import { BookOpen } from "lucide-react";
+import { IconBadge } from "@/components/IconBadge";
+import CourseProgress from "@/app/(course)/courses/[courseId]/_components/CourseProgress";
+import { formatPrice } from "@/lib/formatCurrency";
 
-import CourseProgress from '@/app/(course)/courses/[courseId]/_components/CourseProgress'
-import { IconBadge } from '@/components/IconBadge'
-import { formatPrice } from '@/lib/formatCurrency'
-import { BookOpen } from 'lucide-react'
-import Image from 'next/image'
-import Link from 'next/link'
-import React from 'react'
 
-interface CourseCardProps{
-    id : string
-    title : string
-    imageUrl : string
-    chaptersLength : number,
-    category : string | null
-    price : number | null
-    progress : number | null
-}
-const CourseCard = ({id , title , imageUrl , chaptersLength,category , price,progress} : CourseCardProps) => {
+
+interface CourseCardProps {
+  id: string;
+  title: string;
+  imageUrl: string;
+  chaptersLength: number;
+  price: number;
+  progress: number | null;
+  category: string;
+};
+
+ const CourseCard = ({
+  id,
+  title,
+  imageUrl,
+  chaptersLength,
+  price,
+  progress,
+  category
+}: CourseCardProps) => {
   return (
     <Link href={`/courses/${id}`}>
-  <div className='group h-full p-2 overflow-hidden rounded-lg border'>
-  <div className="carousel border rounded sm:w-full w-full relative aspect-video">
-    <Image  unoptimized={true}className='object-cover' src={imageUrl} alt={title} fill />
-    <h1>{title}</h1>
-
-  </div>
-  <div className='flex flex-col pt-2'>
-    <h1 className='font-bold'>{title}</h1>
-
-  </div>
-  <p className='text-xs text-muted-foreground'>{category}</p>
-<div className="my-3 flex items-center gap-x-3 text-sm">
-  <div className='flex items-center gap-x-1'>
-    <IconBadge size={'sm'} icon={BookOpen}/>
-    <span>
-      {chaptersLength } {chaptersLength === 1 ? "chapter" : "chapters"}
-    </span>
-  </div>
-</div>
-{<CourseProgress variant={progress === 100 ? "success" : "default" } size='sm' value={progress}/>}
-<p className='font-medium'>{formatPrice(price)}</p>
-  </div>
-  
-
+      <div className="group hover:shadow-sm transition overflow-hidden border rounded-lg p-3 h-full">
+        <div className="relative w-full aspect-video rounded-md overflow-hidden">
+          <Image
+            fill
+            className="object-cover"
+            alt={title}
+            src={imageUrl}
+          />
+        </div>
+        <div className="flex flex-col pt-2">
+          <div className="text-lg md:text-base font-medium group-hover:text-sky-700 transition line-clamp-2">
+            {title}
+          </div>
+          <p className="text-xs text-muted-foreground">
+            {category}
+          </p>
+          <div className="my-3 flex items-center gap-x-2 text-sm md:text-xs">
+            <div className="flex items-center gap-x-1 text-slate-500">
+              <IconBadge size="sm" icon={BookOpen} />
+              <span>
+                {chaptersLength} {chaptersLength === 1 ? "Chapter" : "Chapters"}
+              </span>
+            </div>
+          </div>
+          {progress !== null ? (
+            <CourseProgress
+              variant={progress === 100 ? "success" : "default"}
+              size="sm"
+              value={progress}
+            />
+          ) : (
+            <p className="text-md md:text-sm font-medium text-slate-700">
+              {formatPrice(price)}
+            </p>
+          )}
+        </div>
+      </div>
     </Link>
   )
 }
 
-export default CourseCard
+export default CourseCard;
